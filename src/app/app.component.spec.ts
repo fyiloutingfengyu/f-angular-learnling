25ee245b5ac1e3e23e557b13c0ecf17e3b949d1e
fyudeyinji <fyudeyinji@163.com>
2020-07-21 19:00:19 +0800

    完善项目

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {async, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'f-angular-projectc'`, () => {
+  it(`should have as title 'f-angular-learning'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('f-angular-projectc');
+    expect(app.title).toEqual('f-angular-learning');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('f-angular-projectc app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('f-angular-learning app is running!');
   });
 });
