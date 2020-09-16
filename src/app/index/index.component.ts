7bac5bd90ee8e13c0e7ae1306330a12d259247d8
fyudeyinji <fyudeyinji@163.com>
2020-09-16 16:48:44 +0800

    完善打包

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-index',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
+  envObj = environment;
 
-  constructor() { }
+  constructor () {
+  }
 
-  ngOnInit(): void {
+  ngOnInit (): void {
   }
 
 }
