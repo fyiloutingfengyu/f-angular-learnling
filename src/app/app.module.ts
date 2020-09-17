cdc834f1f1aad8160d2ac847dbc0ec9731f7a0de
fyudeyinji <fyudeyinji@163.com>
2020-09-17 14:14:56 +0800

    添加ng-zorro-antd-mobile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgZorroAntdMobileModule } from 'ng-zorro-antd-mobile';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
@@ -16,6 +16,7 @@ import { IndexComponent } from './index/index.component';
   ],
   imports: [
     BrowserModule,
+    NgZorroAntdMobileModule,
     AppRoutingModule
   ],
   providers: [],
